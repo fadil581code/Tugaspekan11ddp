c5ce86358b592873687ebacca0ebdd7e9a08d063
fadil581code <146051075+fadil581code@users.noreply.github.com>
2023-12-11 11:01:44 +0700

    Add files via upload

---
+++ hasil.py
@@ -0,0 +1,24 @@
+from bangun_datar import *
+from aritmatika import *
+
+# hasil bangun_datar
+persegi(5)
+persegi_panjang(5, 2)
+segitiga_sama_sisi(20, 25)
+jajar_genjang(30, 25, 10)
+lingkaran(8)
+belah_ketupat(6, 8, 10)
+layang_layang(4, 6, 8, 10)
+
+# hasil aritmatika 
+penjumlahan(5, 10)
+pengurangan(20, 5)
+perkalian(5, 3)
+pembagian(150, 10)
+modulus(5, 5)
+eksponen(15, 2)
+akar(5, 2)
+pangkat(2, 5)
+median(30, 2)
+taksiran(75, 15)
+pembulatan(10, 2)
---
+++ tugassoal_pekan11aritmatika.py
@@ -0,0 +1,43 @@
+def penjumlahan(bil1, bil2):
+    hasil = bil1 + bil2
+    print("Hasil penjumlahan: ", hasil)
+
+def pengurangan(bil1, bil2):
+    hasil = bil1 - bil2
+    print("Hasil pengurangan: ", hasil)
+
+def perkalian(bil1, bil2):
+    hasil = bil1 * bil2
+    print("Hasil perkalian: ", perkalian)
+
+def pembagian(bil1, bil2):
+    hasil = bil1 / bil2
+    print("Hasil pembagian: ", pembagian)
+
+def modulus(bil1, bil2):
+    modulus = bil1 % bil2
+    print("Hasil modulus: ", modulus)
+
+def eksponen(bil1, bil2):
+    eksponen = bil1 ** bil2
+    print("Hasil eksponen: ", eksponen)
+
+def akar(bil1, bil2):
+    akar = bil1 ** (1/bil2)
+    print("Hasil akar: ", akar)
+
+def pangkat(bil1, bil2):
+    pangkat = bil1 ** bil2
+    print("Hasil pangkat: ", pangkat)
+
+def median(bil1, bil2):
+    median = (bil1 + bil2) / 2
+    print("Hasil median: ", median)
+
+def taksiran(bil1, bil2):
+    taksiran = (bil1 + bil2) / 2
+    print("Hasil taksiran: ", taksiran)
+
+def pembulatan(bil1, bil2):
+    pembulatan = bil1 // bil2
+    print("Hasil pembulatan: ", pembulatan)
